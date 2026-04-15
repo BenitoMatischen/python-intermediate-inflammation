672af0913dfa2dde82f9e4eb09b414ae3ef27ed1
Benito Matischen <benito.matischen@manchester.ac.uk>
2026-04-15 16:19:48 +0100

    Setup tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,8 +2,10 @@
 
 import numpy as np
 import numpy.testing as npt
+import pytest
 
 from inflammation.models import daily_mean
+from inflammation.compute_data import CSVDataSource
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
@@ -29,3 +31,14 @@ def test_daily_mean_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
+@pytest.mark.parametrize(
+    "test_input, expected",
+    [
+        (np.array([[0, 0], [0, 0], [0, 0]]), np.array([0, 0])),
+        (np.array([[1, 2], [3, 4], [5, 6]]), np.array([3, 4])),
+    ]
+)
+def test_load_inflammation_data(test_input, expected):
+    data_source = CSVDataSource('data/')
+    data = data_source.load_inflammation_data()
+    assert len(data) == 12
