c880b6d5cfc22acb622ce52d7373e642150c82bb
Benito Matischen <benito.matischen@manchester.ac.uk>
2026-04-16 10:47:13 +0100

    Don't load data in compute std func to make it purer

--- inflammation/compute_data.py
+++ inflammation/compute_data.py
@@ -23,16 +23,15 @@ class CSVDataSource:
             raise ValueError(f"No inflammation data CSV files found in path {self.__data_dir}")
         return [np.array(models.load_csv(file_path)) for file_path in data_file_paths]
 
-def compute_standard_deviation_by_day(data_source : CSVDataSource) -> np.ndarray:
+def compute_standard_deviation_by_day(data : list[np.ndarray]) -> np.ndarray:
     """Calculates the standard deviation by day between datasets.
 
-    Gets all the inflammation data from CSV files within a directory,
+    Given inflammation data from datasets,
     works out the mean inflammation value for each day across all datasets,
     then calculates the standard deviation of these means.
 
     :returns: 1D Numpy array containing the standard deviation by day
     """
-    data = data_source.load_inflammation_data()
 
     means_by_day = map(models.daily_mean, data)
     means_by_day_matrix = np.stack(list(means_by_day))
@@ -50,6 +49,8 @@ def analyse_data(data_source : CSVDataSource) -> np.ndarray:
 
     :returns: 1D Numpy array containing the standard deviation by day
     """
-    daily_standard_deviation = compute_standard_deviation_by_day(data_source)
+
+    data = data_source.load_inflammation_data()
+    daily_standard_deviation = compute_standard_deviation_by_day(data)
 
     return daily_standard_deviation
